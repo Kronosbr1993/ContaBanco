13196682e37d95895482867708661ec376f758aa
Kronos <thiagoriegeldemoura@gmail.com>
2025-05-17 19:50:58 -0300

    projeto bancom concluido

--- ContaTerminal/src/ContaTerminal.java
+++ ContaTerminal/src/ContaTerminal.java
@@ -1,18 +1,27 @@
 
-    public class ContaTerminal {
-        public static void main(String[] args) {
-            // Informações pré-definidas
-            int numero = 1021;
-            String agencia = "067-8";
-            String nomeCliente = "MARIO ANDRADE";
-            double saldo = 237.48;
-            
-            System.out.println("Por favor, digite o número da Agência:");
-            String agenciaDigitada = System.console().readLine();
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+        
+        System.out.println("Por favor, digite o saldo:");
+        double saldo = scanner.nextDouble();// Consumir a quebra de linha
 
 
-            // Mensagem final
-            String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
-            System.out.println(mensagem);
-        }
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
+        System.out.println(mensagem);
+
+        scanner.close();
     }
+}
